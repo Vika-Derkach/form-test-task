6de0454085ce7e444bbe15571614295fceb5dd71
Vika-Derkach <queenviktoriyphp@gmail.com>
2022-04-13 20:05:41 +0300

    add forwardref

--- src/components/SelectPayment/SelectPayment.tsx
+++ src/components/SelectPayment/SelectPayment.tsx
@@ -1,5 +1,5 @@
 import cn from "classnames";
-import React from "react";
+import React, { ForwardedRef, forwardRef } from "react";
 import { Controller } from "react-hook-form";
 import Select, { components } from "react-select";
 import { ReactComponent as ArrowIcon } from "./arrow.svg";
@@ -11,91 +11,89 @@ interface IOption {
   label: string;
 }
 
-export const SelectPayment = ({
-  className,
-  name,
-  error,
-  onChange,
-  control,
-  ...props
-}: SelectProps): JSX.Element => {
-  const options: IOption[] = [
-    { value: SelectEnum.Cash, label: "Cash" },
-    { value: SelectEnum.Checks, label: "Checks" },
-    { value: SelectEnum.CreditCards, label: "Credit cards" },
-  ];
+export const SelectPayment = forwardRef(
+  (
+    { className, name, error, onChange, control, ...props }: SelectProps,
+    ref: ForwardedRef<HTMLInputElement>
+  ): JSX.Element => {
+    const options: IOption[] = [
+      { value: SelectEnum.Cash, label: "Cash" },
+      { value: SelectEnum.Checks, label: "Checks" },
+      { value: SelectEnum.CreditCards, label: "Credit cards" },
+    ];
 
-  const DropdownIndicator = (props: any) => {
+    const DropdownIndicator = (props: any) => {
+      return (
+        <components.DropdownIndicator {...props}>
+          <ArrowIcon />
+        </components.DropdownIndicator>
+      );
+    };
+
     return (
-      <components.DropdownIndicator {...props}>
-        <ArrowIcon />
-      </components.DropdownIndicator>
-    );
-  };
+      <div className={cn(className)}>
+        <Controller
+          control={control}
+          render={({ field: { onChange, value, name, ref } }) => {
+            const currentSelection = options.find((c) => c.value === value);
 
-  return (
-    <div className={cn(className)}>
-      <Controller
-        control={control}
-        render={({ field: { onChange, value, name, ref } }) => {
-          const currentSelection = options.find((c) => c.value === value);
+            const handleSelectChange = (selectedOption: IOption | null) => {
+              onChange(selectedOption?.value);
+            };
 
-          const handleSelectChange = (selectedOption: IOption | null) => {
-            onChange(selectedOption?.value);
-          };
-
-          return (
-            <Select
-              inputRef={ref}
-              value={currentSelection}
-              name={name}
-              components={{ DropdownIndicator }}
-              options={options}
-              onChange={handleSelectChange}
-              styles={{
-                indicatorSeparator: () => ({
-                  display: "none",
-                }),
-                placeholder: (base) => ({
-                  ...base,
-                  ...styles.placeholder,
-                }),
-                container: (base) => ({
-                  ...base,
-                  ...styles.select,
-                }),
-                control: (base) => ({
-                  ...base,
-                  ...styles.control,
-                }),
-
-                option: (base, { isDisabled, isFocused, isSelected }) => {
-                  return {
+            return (
+              <Select
+                inputRef={ref}
+                value={currentSelection}
+                name={name}
+                components={{ DropdownIndicator }}
+                options={options}
+                onChange={handleSelectChange}
+                styles={{
+                  indicatorSeparator: () => ({
+                    display: "none",
+                  }),
+                  placeholder: (base) => ({
                     ...base,
-                    ...styles.options,
-                    backgroundColor: isDisabled
-                      ? undefined
-                      : isSelected
-                      ? "var(--primary)"
-                      : undefined,
-                    color: isDisabled
-                      ? "#ccc"
-                      : isSelected
-                      ? "var(--white)"
-                      : "var(--black)",
+                    ...styles.placeholder,
+                  }),
+                  container: (base) => ({
+                    ...base,
+                    ...styles.select,
+                  }),
+                  control: (base) => ({
+                    ...base,
+                    ...styles.control,
+                  }),
 
-                    cursor: isDisabled ? "not-allowed" : "default",
-                  };
-                },
-              }}
-            />
-          );
-        }}
-        name="paymentMethod"
-        rules={{
-          required: true,
-        }}
-      />
-    </div>
-  );
-};
+                  option: (base, { isDisabled, isFocused, isSelected }) => {
+                    return {
+                      ...base,
+                      ...styles.options,
+                      backgroundColor: isDisabled
+                        ? undefined
+                        : isSelected
+                        ? "var(--primary)"
+                        : undefined,
+                      color: isDisabled
+                        ? "#ccc"
+                        : isSelected
+                        ? "var(--white)"
+                        : "var(--black)",
+
+                      cursor: isDisabled ? "not-allowed" : "default",
+                    };
+                  },
+                }}
+              />
+            );
+          }}
+          name="paymentMethod"
+          rules={{
+            required: true,
+          }}
+        />
+      </div>
+    );
+  }
+);
